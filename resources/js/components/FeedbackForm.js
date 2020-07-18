af3b18bc12c1e01ee66fd7137e5d18925b2008e1
affi1504 <afrid.afnan71@gmail.com>
2020-07-18 20:07:53 +0530

    ui changed

--- resources/js/components/FeedbackForm.js
+++ resources/js/components/FeedbackForm.js
@@ -1,46 +1,127 @@
-import React, { Component } from 'react'
-import Answer from './Answer';
-import Container from '@material-ui/core/Container';
-import SimpleSelect from './SimpleSelect';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
+import React ,{ useState }  from 'react';
+import { makeStyles,Typography, Select ,Paper ,FormControl ,MenuItem ,InputLabel } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import Rating from '@material-ui/lab/Rating';
 
-
-
-class FeedbackForm extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            branches: [],
-            stores: [],
-            questions:[]
-        }
-     }
-
-    componentDidMount()  {
-        axios.get('http://localhost:8000/api/feedback')
-        .then(res=>{
-            this.setState({
-                branches: res.data.branches,
-                stores: res.data.stores,
-                questions: res.data.questions
-            })
-        })
+const useStyles = makeStyles((theme) => ({
+    buttonStyle: {
+        marginTop: theme.spacing(2),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+      },
+      selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    paperControl: {
+        marginTop: theme.spacing(2),
+        padding: theme.spacing(2),
     }
+  }));
 
+const FeedbackForm = () => {
+    const classes = useStyles();
+
+
+    const [questions, setQuestions] = useState([
+        { id:1, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:2, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:3, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:4, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:5, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:6, ques:"Dolor deserunt tempor consectetur ipsum."},
+        { id:7, ques:"Dolor deserunt tempor consectetur ipsum."},
+    ]);
+
+    const [branches, setBranches] = useState([
+        { name:'mangalore' , id: 1 },
+        { name:'udupi' , id: 2 },
+        { name:'surathkal' , id: 3 }
+    ]);
+    
+      const [stores, setStores] = useState([
+        { name: 'falnir', branch_id: 1 , id: 1 },
+        { name: 'pandeshwar', branch_id: 1 , id: 2 },
+        { name: 'atttawar', branch_id: 1 , id: 3 },
+        { name: 'udupi', branch_id: 2 , id: 4 },
+        { name: 'manipal', branch_id: 2 , id: 5 },
+        { name: 'krishnapur', branch_id: 3 , id: 6 },
+        { name: 'katipalla', branch_id: 3 , id: 7 },
+      ]);
+    
+    const [currentStore, setCurrentStore] = useState('');
+    
+    const [currentBranch, setcurrentBranch] = useState('');
+
+      const handleBranchChange = (event) => {
+        setcurrentBranch(event.target.value);
+      };
+    
+      const handleStoreChange = (event) => {
+        setCurrentStore(event.target.value);
+    };
+    
+    const handleChange = (id, value) => {
+        value? (console.log("for question with id " + id + " the rating is " + value))
+            : (
+                console.log("please qive some rating")
+            )
+    }
     
 
-
-render(){
-    return(
-        <Container maxWidth="sm">
-            <form>
-                <p>{this.state.questions}</p>
-            </form>
-        </Container>
-    )
+    const handleClick = () => {
+        console.log(currentBranch, currentStore);
     }
-}
+    return ( 
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="branch">Branch </InputLabel>
+                <Select labelId="branch" id="simple-select-branch" value={currentBranch} onChange={handleBranchChange} >
+                    {branches.map(branch => {
+                        return (
+                        <MenuItem key= {branch.id} value={branch.name}>{ branch.name}</MenuItem>
+                        )
+                    }) }
+                </Select>
+            </FormControl>
 
-export default FeedbackForm
+            <FormControl className={classes.formControl}>
+                <InputLabel id="store">Store</InputLabel>
+                <Select labelId="store" id="simple-select-store" value = { currentStore } onChange={handleStoreChange} >
+                    {stores.map(store => {
+                        return (
+                        <MenuItem key= {store.id} value={store.name}>{ store.name}</MenuItem>
+                        )
+                    }) }
+                </Select>
+            </FormControl>
+
+            {questions.map(question => {
+                return (
+                    <Paper key={question.id} elevation={3} className={classes.paperControl} >
+                        <Typography>{question.ques}</Typography>
+                        <Rating required size="large" onChange={(e, value) => handleChange(question.id, value)} />
+                    </Paper>
+                )
+            })
+            }
+        
+            <Button variant="contained" color="primary" className={ classes.buttonStyle} onClick ={ handleClick }> Primary </Button>
+        </div> 
+     );
+}
+ 
+export default FeedbackForm;
+
+
+
+
+
+
+
+
+
+
+
+    
--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Header from './Header';
+import Navbar from './Navbar'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import FeedbackForm from './FeedbackForm';
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: 30,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-
-class Index extends Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <div>
-        <Header />
-        <FeedbackForm />
-        </div>
-      </BrowserRouter>
-    )
+const Index = () => {
+  const classes = useStyles();
+  return (
+    <div className="App">
+      <Navbar />  
+      <Grid container >
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>
+            <FeedbackForm />
+          </Paper>
+        </Grid>
+      </Grid>
+  </div>
+  );
   }
-}
+
 
 ReactDOM.render(<Index />, document.getElementById('app'))
